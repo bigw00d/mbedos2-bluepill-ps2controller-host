0ba0da5fb1a070fca92de02aa9c4edbb71183e24
daiki <daiking_tensai@yahoo.co.jp>
2020-05-04 14:17:24 +0900

    add mouseModeFunction(no click)

--- main.cpp
+++ main.cpp
@@ -2,13 +2,21 @@
 #include "mbed.h"
 #include "USBJoystick.h"
 #include "PS_PAD.h"
+#include "USBMouseKeyboard.h"
 
 #define BUTTON_NUM 8
+#define COUNT_3_SEC 30
 
 DigitalOut led(PC_13);
 Serial      pc(PA_2, PA_3); // TX, RX
 // PS_PAD      ps2(PA_7, PA_6, PA_5, PB_6);  //mosi=CMD,miso=DAT,clk=CLK,ss=SEL
 // USBJoystick joystick;
+
+static Timer timer;
+static Ticker ticker;
+static uint8_t ticks = 0;
+static uint32_t longPushCnt = 0;
+static uint32_t oldLongPushCnt = 0;
 
 int16_t moveTable[16][2] = {
     {0         , 0           },	//	NEUTRAL(0b0000)    
@@ -24,6 +32,30 @@ int16_t moveTable[16][2] = {
     {0         , 0           },	//	---(0b1010)    
     {0         , 0           },	//	---(0b1011)    
     {JX_MAX_ABS, JY_MIN_ABS  },	//	UPRIGHT(0b1100)   
+    {0         , 0           },	//	---(0b1101)    
+    {0         , 0           },	//	---(0b1110)    
+    {0         , 0           },	//	---(0b1111)    
+};
+
+#define MX_MIN_ABS    (-10)
+#define MY_MIN_ABS    (-10)
+#define MX_MAX_ABS    (20)
+#define MY_MAX_ABS    (20)
+
+int16_t moveMouseTable[16][2] = {
+    {0         , 0           },	//	NEUTRAL(0b0000)    
+    {MX_MIN_ABS, 0           },	//	LEFT(0b0001)       
+    {0         , MY_MAX_ABS  },	//	DOWN(0b0010)       
+    {MX_MIN_ABS, MY_MAX_ABS  },	//	DOWNLEFT(0b0011)  
+    {MX_MAX_ABS, 0           },	//	RIGHT(0b0100)      
+    {0         , 0           },	//	---(0b0101)    
+    {MX_MAX_ABS, MY_MAX_ABS  },	//	RIGHTDOWN(0b0110) 
+    {0         , 0           },	//	---(0b0111)    
+    {0         , MY_MIN_ABS  },	//	UP(0b1000)         
+    {MX_MIN_ABS, MY_MIN_ABS  },	//	LEFTUP(0b1001)    
+    {0         , 0           },	//	---(0b1010)    
+    {0         , 0           },	//	---(0b1011)    
+    {MX_MAX_ABS, MY_MIN_ABS  },	//	UPRIGHT(0b1100)   
     {0         , 0           },	//	---(0b1101)    
     {0         , 0           },	//	---(0b1110)    
     {0         , 0           },	//	---(0b1111)    
@@ -45,6 +77,18 @@ uint32_t ps2tojoypad(int ps2movebtn);
 void joypadMode(void);
 void mouseMode(void);
 
+static void tickHandler( void ) {
+    // ticks += timer.read_us( ) >> 6;
+    // timer.reset( );
+    if (longPushCnt > oldLongPushCnt) {
+        ticks++;
+    }
+    else {
+        ticks = 0;
+    }
+    oldLongPushCnt = longPushCnt;
+}
+
 int main() {
     // uint8_t ps2move = 0;    
     // int16_t x = 0;
@@ -61,7 +105,14 @@ int main() {
     // led = 1; //led off
     led = 0; //led on
 
-    joypadMode();
+     __disable_irq( );
+    // configure timer
+    // timer.start( );
+    ticker.attach_us( tickHandler, 100000 ); // reset timer every 100msec
+     __enable_irq( );
+
+    // joypadMode();
+    mouseMode();
 
     // while(1)
     // {   
@@ -83,7 +134,53 @@ int main() {
 }
 
 void mouseMode() {
-    ;
+    USBMouseKeyboard key_mouse;
+    PS_PAD      ps2(PA_7, PA_6, PA_5, PB_6);  //mosi=CMD,miso=DAT,clk=CLK,ss=SEL
+    // USBJoystick joystick;
+
+    uint8_t ps2move = 0;    
+    int16_t x = 0;
+    int16_t y = 0;
+    uint32_t buttons = 0;    
+    int ps2movebtn = 0;
+
+    ps2.init();
+
+    while(1)
+    {
+        // check ticks
+        if(ticks >= COUNT_3_SEC) { // 3sec
+            ticks = 0;
+            led = !led; // toggle led
+        }
+
+        ps2.poll();     
+
+        // check button
+        ps2movebtn = ps2.read(PS_PAD :: BUTTONS);
+        buttons = ps2tojoypad(ps2movebtn);
+        
+        if ( buttons & 0x0080 ) { // START
+            if ( buttons & 0x0040 ) { // SELECT
+                longPushCnt++;
+            }
+            else {
+                longPushCnt = 0;
+            }
+        }
+        else {
+            longPushCnt = 0;
+        }
+
+        // check move
+        ps2move = ps2.read_move();
+        ps2move &= 0x0F;
+        x = moveMouseTable[ps2move][0];  // value -127 .. 128
+        y = moveMouseTable[ps2move][1];  // value -127 .. 128
+        // joystick.move_buttons(x, y, buttons);
+        key_mouse.move(x, y);
+        wait(0.05);
+    }
 }
 
 void joypadMode() {
@@ -99,13 +196,31 @@ void joypadMode() {
     ps2.init();
 
     while(1)
-    {   
+    {
+        // check ticks
+        if(ticks >= COUNT_3_SEC) { // 3sec
+            ticks = 0;
+            led = !led; // toggle led
+        }
+
         ps2.poll();     
 
         // check button
         ps2movebtn = ps2.read(PS_PAD :: BUTTONS);
         buttons = ps2tojoypad(ps2movebtn);
         
+        if ( buttons & 0x0080 ) { // START
+            if ( buttons & 0x0040 ) { // SELECT
+                longPushCnt++;
+            }
+            else {
+                longPushCnt = 0;
+            }
+        }
+        else {
+            longPushCnt = 0;
+        }
+
         // check move
         ps2move = ps2.read_move();
         ps2move &= 0x0F;
